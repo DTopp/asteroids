666dec420d95602ffb4838ec3189833fee3b2004
DTopp <danieltopp@me.com>
2025-04-09 14:18:13 +0100

    Ship forward/reverse and player groups added

--- main.py
+++ main.py
@@ -7,14 +7,21 @@ def main():
     pygame.init()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)
         screen.fill("black")  # Fill the screen with black
-        player.update(dt)
-        player.draw(screen)
+        
+        for sprite in drawable:
+            sprite.draw(screen)
         
         pygame.display.flip()
         dt = clock.tick(60) / 1000 # Convert milliseconds to seconds
