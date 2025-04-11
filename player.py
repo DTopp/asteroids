941238e136a6d8f2bb84d19ff8817cdd7afd86ca
DTopp <danieltopp@me.com>
2025-04-11 09:34:44 +0100

    Shots tweaked

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -34,14 +35,16 @@ class Player(CircleShape):
             self.move(-dt)
         if keys[pygame.K_SPACE]:
             self.shoot()
+        self.timer -= dt
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        shot = Shot(self.position.x + forward.x,
-                    self.position.y + forward.y)
-        shot.velocity = forward * PLAYER_SHOOT_SPEED
-        return shot
+        
+        if self.timer > 0:
+            return
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        self.timer = PLAYER_SHOOT_COOLDOWN
